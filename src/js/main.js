308f139bd21b018d27605fe5d8004eb69771521c
Bus-D <gar20057@byui.edu>
2025-09-20 21:25:29 -0600

    completed NPS - part 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner-title">${info.name}</a>
+    <p class="hero-banner-text">
+        <span>${info.designation}</span>
+        <span>${info.states}</span>`
+}
+
+function titleUpdate(info) {
+    document.title = info.fullName
+}
+
+titleUpdate(parkData);
+
+const heroInfo = document.querySelector(".hero-banner-content");
+heroInfo.innerHTML = parkInfoTemplate(parkData);
+
+const heroImg = document.querySelector(".hero-banner > img");
+heroImg.src = parkData.images[0].url;
+heroImg.alt = parkData.images[0].altText;
